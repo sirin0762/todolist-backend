d3ec29e25128c49f83eb4b80bffe9f78258f25fe
sirin0762 <tlsdydwls123@gmail.com>
2023-12-19 21:07:59 +0900

    CHORE: cors allowed method 삭제

--- src/main/java/sirin/example/todolistbackend/config/WebMvcConfiguration.java
+++ src/main/java/sirin/example/todolistbackend/config/WebMvcConfiguration.java
@@ -24,8 +24,8 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://todolist-frontend-sirin0762.vercel.app/") // 허용할 출처
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP method
+            .allowedOrigins("https://todolist-frontend-sirin0762.vercel.app") // 허용할 출처
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP method
             .allowCredentials(true) // 쿠키 인증 요청 허용
             .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
